c9f8ad3dc3c3bd3c161f97f03454cbc5ee7b9886
igorl <igorl@DESKTOP-273U8TR>
2024-05-22 17:42:38 -0300

    Adicionar arquivos de projeto.

---
+++ problema-menor-de-três/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_menor_de_três {
+    internal class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int a, b, c, menor;
+
+            Console.Write("Primeiro valor: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Terceiro valor: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a < b && a < c) {
+                menor = a;
+            }
+            else if (b < c) {
+                menor = b;  
+            }
+            else {
+                menor = c;
+            }
+
+            Console.WriteLine("MENOR VALOR DIGITADO = " + menor);
+        }
+    }
+}
